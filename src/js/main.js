e8de0f58bc9f878966f925b34fb36bbec6115ea1
Thomas Wilburn <news@thomaswilburn.net>
2016-09-22 14:39:50 -0700

    Ready

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+// require("./lib/social");
+// require("./lib/ads");
+// var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+var template = require("./lib/dot").compile(require("./_card.html"));
+var $ = require("./lib/qsa");
+
+var shuffle = require("lodash.shuffle");
+var strings = require("./strings.txt").split("\n").filter(s => s);
+
+var makeCard = function() {
+  var selected = shuffle(strings).slice(0, 24);
+
+  document.querySelector(".card-container").innerHTML = template(selected);
+
+  $(".cell").forEach(el => el.addEventListener("click", function() { this.classList.toggle("tapped") }));
+};
+
+makeCard();
+document.querySelector(".reshuffle").addEventListener("click", makeCard);
